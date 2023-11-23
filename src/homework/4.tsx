9ea7d9d036af06bac5d510fcdf58a41ae302de4b
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-11-23 23:19:39 +0200

    update

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -5,7 +5,7 @@ type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 type SelectedMenu = { id: MenuIds } | object;
 type MenuSelected = {
-  selectedMenu: object;
+  selectedMenu: SelectedMenu;
 };
 type MenuAction = {
   onSelectedMenu: (obj: SelectedMenu) => void;
